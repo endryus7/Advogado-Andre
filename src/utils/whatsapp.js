6a5e19303264c2ad04ff6f07cdf97adf61c00b6f
Endryus-Montezano <111534901+endryus7@users.noreply.github.com>
2026-07-31 14:15:29 -0300

    Modifica mensagem do whatsapp

--- src/utils/whatsapp.js
+++ src/utils/whatsapp.js
@@ -1,6 +1,6 @@
 export const WHATSAPP_NUMBER = "555192605349";
 export const WHATSAPP_MESSAGE =
-  "Olá, Dr. André Albani Lara.\nGostaria de falar sobre um caso de Direito Penal.";
+  "Olá, Dr. André Albani Lara.\nGostaria de falar sobre um caso.";
 export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(
   WHATSAPP_MESSAGE
 )}`;
